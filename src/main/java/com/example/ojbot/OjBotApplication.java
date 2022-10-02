4da44f7d9a22cd45b8d4d706118e0abe3cd608e6
tzihklearn <1922941549@qq.com>
2022-10-02 12:16:49 +0800

    feat：初始化

---
+++ src/main/java/com/example/ojbot/OjBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.ojbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OjBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ojbot/OjBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ojbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
